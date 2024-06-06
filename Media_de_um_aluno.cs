1583f5bc3077598a5c93bb843648102ec892adb1
Lucas Carvalho <161785293+carvalhosouzalucas@users.noreply.github.com>
2024-06-06 15:14:40 -0300

    Add files via upload

---
+++ Media_de_um_aluno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Media
+{
+    static void Main()
+    {
+        float nota1;
+        float nota2;
+        float nota3;
+        float nota4;
+        float resultado;
+        float media;
+
+        Console.WriteLine("Digite a primeira nota: ");
+        nota1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a segunda nota: ");
+        nota2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a terceira nota: ");
+        nota3 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a quarta nota: ");
+        nota4 = float.Parse(Console.ReadLine());
+
+        resultado = nota1 + nota2 + nota3 + nota4;
+
+        media = resultado / 4;
+
+        Console.WriteLine("A média das notas é: " + media);
+    }
+}
+
+
